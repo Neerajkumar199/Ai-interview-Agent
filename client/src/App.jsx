ae75141cdc8858a6ab6a2c5289c6488b76197a52
Neerajkumar199 <145154329+Neerajkumar199@users.noreply.github.com>
2026-05-11 14:58:01 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://ai-interview-agent-rayz.onrender.com"
+export const ServerUrl  = "https://ai-interview-agent-1backend.onrender.com"
 
 function App() {
 
